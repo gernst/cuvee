6ff8dfcdd3446edef277b68369efda9fb577435c
Daniel Albert <git@esclear.de>
2022-06-28 11:04:06 +0200

    Fix proof obligations generated by induciton tactic

--- cuvee/src/cuvee/backend/Tactic.scala
+++ cuvee/src/cuvee/backend/Tactic.scala
@@ -35,16 +35,17 @@ case class Induction(variable: Var, cases: List[(Expr, Tactic)]) extends Tactic 
 
     // Build a formula that states the induction hypothesis, i.e. forall m : int :: m < variable ==> P(m)
     // TODO: Actually choose a new name, not just "m" → use prop
-    val n = variable;
-    val m = Var(variable.name.withName("m"), Sort.int)
+    val n = variable
+    val m = variable.prime
+
     val lt = App(Inst(Fun("<", Nil, List(Sort.int, Sort.int), Sort.bool), Map()), List(m, n))
-    val ind = Bind(Quant("forall"), List(), Imp(lt, prop.subst(Map(variable -> m)).toExpr), Sort.bool)
+    val ind = Bind(Quant("forall"), List(m), Imp(lt, prop_.subst(Map(variable -> m)).toExpr), Sort.bool)
 
     // TODO: Once constructors / datatypes are available, generate a list of all constructors and handle undefined cases automatically
 
     List(
       (prop_.subst(Map(variable -> Lit(0, Sort.int))), Some(cases(0)._2)),
-      (Disj.show(List(ind), xs, pos, neg), Some(cases(1)._2))
+      (Disj.assume(List(ind), Nil, xs, pos, neg), Some(cases(1)._2))
     )
   };
 }
