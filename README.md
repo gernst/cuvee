5c9201f9eded8c9bb442de1bc0e53f3154c52ab9
Gidon Ernst <gidon.ernst@sosy.ifi.lmu.de>
2022-10-29 16:41:16 +0200

    add mill, change pipeline

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# cuvee
-A tool for Boogie and SMT-LIB engineering
+# Cuvée
+
+Cuvée is a tool for specification and proof engineering.
+It works on an intermediate language that is very close to [SMT-LIB](smtlib.cs.uiowa.edu/),
+but accepts a subset of the [Boogie language](https://github.com/boogie-org), too.
+
+
--- cuvee/src/cuvee/Cuvee.scala
+++ cuvee/src/cuvee/Cuvee.scala
@@ -18,60 +18,80 @@ object Cuvee extends Main {
   def main(args: Array[String]) {
     val c = new Cuvee
     c.configure(args.toList)
-    c.run(c.cmds, c.state.get, z3(c.state.get))
+    c.run(c.cmds, c.state, z3(c.state))
   }
 }
 
 class Cuvee {
-  var state: Option[State] = None
+  var state = State.default
   var cmds: List[Cmd] = Nil
   var printer: Printer = cuvee.smtlib.printer
   var rewrite: Boolean = false
+
+  val options = Map(
+    "-help" ->
+      ("show help text", () => { help() }),
+    "-debug:solver" ->
+      ("show information on the interaction with the backend", () => {
+        cuvee.smtlib.solver.debug = true
+      }),
+    "-debug:scanner" -> ("show list of parsed tokens (SMT-LIB only)", () => {
+      cuvee.sexpr.debug = true
+    }),
+    "-debug:prover" -> ("show details about proof steps and tactic applications", () =>
+      {
+        cuvee.util.Proving.debug = true
+      }),
+    "-print:smtlib" -> ("override printer to output SMT-LIB format", () => {
+      this.printer = cuvee.smtlib.printer
+    }),
+    "-print:boogie" -> ("override printer to output Boogie format", () => {
+      this.printer = cuvee.boogie.printer
+    }),
+    "-rewrite" -> ("apply structurally recursive axioms as rewrite rules", () =>
+      {
+        this.rewrite = true
+      })
+  )
+
+  def help() {
+    println("cuvee [options] <file>")
+    for ((flag, (description, _)) <- options) {
+      println("  " + flag)
+      println("      " + description)
+      println()
+    }
+  }
 
   def configure(args: List[String]) {
     args match {
       case Nil =>
 
-      case "-debug:solver" :: rest =>
-        cuvee.smtlib.solver.debug = true
+      case first :: rest if options contains first =>
+        val (_, action) = options(first)
+        action()
         configure(rest)
-
-      case "-debug:scanner" :: rest =>
-        cuvee.sexpr.debug = true
-        configure(rest)
-
-      case "-debug:prover" :: rest =>
-        cuvee.util.Proving.debug = true
-        configure(rest)
-
-      case "-print:smtlib" :: rest =>
-        printer = cuvee.smtlib.printer
-        configure(rest)
-
-      case "-print:boogie" :: rest =>
-        printer = cuvee.boogie.printer
-        configure(rest)
-
-      case path :: rest if state.isDefined =>
-        cuvee.error(
-          "A file was already loaded. At the moment only a single input file is supported."
-        )
 
       case "-rewrite" :: rest =>
         rewrite = true
         configure(rest)
 
+      case path :: rest if cmds.nonEmpty =>
+        cuvee.error(
+          "A file was already loaded. At the moment only a single input file is supported."
+        )
+
       case path :: rest if path.endsWith(".bpl") =>
         val (cmds_, state_) = cuvee.boogie.parse(path)
         printer = cuvee.boogie.printer
-        state = Some(state_)
+        state = state_
         cmds = cmds_
         configure(rest)
 
       case path :: rest if path.endsWith(".smt2") =>
         val (cmds_, state_) = cuvee.smtlib.parse(path)
         printer = cuvee.smtlib.printer
-        state = Some(state_)
+        state = state_
         cmds = cmds_
         configure(rest)
 
@@ -112,7 +132,7 @@ class Cuvee {
   // }
 
   def run(cmds: List[Cmd], state: State, solver: Solver) {
-    assert(this.state.isDefined, "No file was parsed")
+    // assert(cmds.nonEmpty, "No file was parsed")
 
     solver.exec(SetOption("produce-models", true))
     solver.exec(SetOption("produce-assignments", true))
@@ -163,9 +183,9 @@ class Cuvee {
           solver.assert(phi)
 
         case Lemma(expr, tactic) =>
-          println()
-          println("================  LEMMA  ================")
-          println("show:  " + expr)
+          // println()
+          // println("================  LEMMA  ================")
+          // println("show:  " + expr)
 
           Proving.show(Disj.from(expr), tactic)(
             state,
