d7dec7936b8ffa8727b171f233f1c9227e181a8d
Gidon Ernst <gidon.ernst@sosy.ifi.lmu.de>
2023-06-24 00:49:16 +0200

    some more interesting conditional recognition

---
+++ cuvee/src/cuvee/lemmas/Conditional.scala
@@ -0,0 +1,102 @@
+package cuvee.lemmas
+
+import cuvee.pure._
+import cuvee.util.Name
+import scala.collection.mutable.ListBuffer
+
+object Conditional {
+  def checkIdentityWithParamPicksAndGuard(df: Def): List[(Rule, Def)] = {
+    var Def(f, cases) = df
+    var Fun(name, _, args, res) = f
+
+    val results =
+      for (
+        (arg, n) <- args.zipWithIndex if arg == res;
+        res <- checkCasesAgainstIdentAtAndGeneratePre(cases, n, f)
+      )
+        yield res
+
+    return results
+  }
+
+  def checkCasesAgainstIdentAtAndGeneratePre(
+      cases: List[C],
+      pos: Int,
+      recfunc: Fun
+  ): Option[(Rule, Def)] = {
+    var Fun(recfuncname, recfuncparams, recfuncargs, recfuncret) = recfunc
+    var prefun = Fun(Name(recfuncname + "_id?", Some(pos)), recfuncparams, recfuncargs, Sort.bool)
+
+    var stuff =
+      for (c @ C(caseargs, caseguard, casebody) <- cases)
+        yield checkCaseAgainstIdentAt(c, pos, recfunc) match {
+          // case (false,Nil) => false
+          case None =>
+            false -> C(caseargs, caseguard, False)
+
+          case Some(recarglists) =>
+            val recs = recarglists map (App(prefun, _))
+            true -> C(caseargs, caseguard, And(recs))
+        }
+
+    val (kept, precases) = stuff.unzip
+
+    if (kept exists (b => b)) {
+      val xs = Expr.vars("x", recfuncargs)
+      val cond = App(prefun, xs)
+      val lhs = App(recfunc, xs)
+      val rhs = xs(pos)
+      val eq = Rule(lhs, rhs, cond)
+      val defpre = Def(prefun, precases.toList)
+      Some((eq, defpre))
+    } else {
+      None
+    }
+  }
+
+  def checkCaseAgainstIdentAt(c: C, pos: Int, recfunc: Fun): Option[List[List[Expr]]] = {
+    val C(args, guard, body) = c
+    val argPick = args(pos)
+
+    argPick match {
+      case arg: Var if arg == body =>
+        Some(Nil) // ok but no recursive arguments
+
+      case arg: Lit if arg == body =>
+        Some(Nil)
+
+      case App(f, pAppParamList) =>
+        body match {
+          case App(g, bAppParamList) if f == g => {
+            val stuff = (pAppParamList zip bAppParamList) map {
+              case (pAppParam: Var, bAppParam) if pAppParam == bAppParam =>
+                true -> None
+              case (pAppParam: Lit, bAppParam) if pAppParam == bAppParam =>
+                true -> None
+              case (pAppParam: Var, App(Inst(`recfunc`, _), recargs))
+                  if pAppParam == recargs(pos) =>
+                true -> Some(recargs)
+              case (pAppParam: Lit, App(Inst(`recfunc`, _), recargs))
+                  if pAppParam == recargs(pos) =>
+                true -> Some(recargs)
+              case other =>
+                false -> None
+            }
+
+            val (oks, recargs) = stuff.unzip
+            if (oks forall (b => b)) {
+              Some(recargs.flatten)
+            } else {
+              None
+            }
+          }
+
+          case other =>
+            None
+        }
+
+      case other =>
+        None
+    }
+  }
+}
--- examples/smtlib/remove.smt2
+++ examples/smtlib/remove.smt2
@@ -5,8 +5,31 @@
   ((par (A) ((nil) (cons (head A) (tail (Lst A)))))))
 
 
+(declare-fun contains (Elem (Lst Elem)) Bool)
 (declare-fun append ((Lst Elem) (Lst Elem)) (Lst Elem))
 (declare-fun remove (Elem (Lst Elem)) (Lst Elem))
+
+
+(declare-fun not_ (Bool) Bool)
+(assert (= (not_ true) false))
+(assert (= (not_ false) true))
+
+(assert
+  (forall
+    ((x Elem))
+    (=
+      (contains x nil)
+      false)))
+(assert
+  (forall
+    ((x Elem) (y Elem) (ys (Lst Elem)))
+    (=
+      (contains x (cons y ys))
+      (or
+        (=
+          x
+          y)
+        (contains x ys)))))
 
 (assert
   (forall
