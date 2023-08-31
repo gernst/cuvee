c92199c018532f97a0529e6dc691c8f42036fb22
Hannah <knie.hannah@campus.lmu.de>
2023-08-31 08:49:27 +0200

    rm `def sexpr` in `smtlib/Cmd.scala`

--- cuvee/src/cuvee/smtlib/Cmd.scala
+++ cuvee/src/cuvee/smtlib/Cmd.scala
@@ -3,7 +3,6 @@ package cuvee.smtlib
 import cuvee.pure._
 
 import cuvee.boogie
-import cuvee.sexpr
 import scala.reflect.ClassTag
 import cuvee.prove.Tactic
 import cuvee.imp.Prog
@@ -16,30 +15,26 @@ sealed trait Res extends util.Syntax {}
 sealed trait IsSat extends Res
 sealed trait Ack extends Res
 
-case object Success extends Ack { def sexpr = "success" }
-case object Unsupported extends Ack { def sexpr = "unsupported" }
+case object Success extends Ack
+case object Unsupported extends Ack
 
-case class Error(info: List[Any]) extends Exception with Res {
-  def sexpr = "error" :: info
-}
-
+case class Error(info: List[Any]) extends Exception with Res {}
 object Error extends (List[Any] => Error) {
   def apply(info: List[Any]): Nothing = {
     throw new Error(info)
   }
 }
 
-case object Sat extends IsSat { def sexpr = "sat" }
-case object Unsat extends IsSat { def sexpr = "unsat" }
-case object Unknown extends IsSat { def sexpr = "unknown" }
+case object Sat extends IsSat
+case object Unsat extends IsSat
+case object Unknown extends IsSat
 
 case class Model(defs: List[DefineFun]) extends Res {
-  def sexpr = "model" :: defs
-
   def rename(re: Map[Var, Var]): Model = {
-    val defs_ = defs map { case DefineFun(name, params, formals, res, body, rec) =>
-      val name_ = re get (Var(name, res)) map (_.name) getOrElse name
-      DefineFun(name_, params, formals rename re, res, body rename re, rec)
+    val defs_ = defs map {
+      case DefineFun(name, params, formals, res, body, rec) =>
+        val name_ = re get (Var(name, res)) map (_.name) getOrElse name
+        DefineFun(name_, params, formals rename re, res, body rename re, rec)
     }
     Model(defs_)
   }
@@ -55,18 +50,15 @@ sealed trait Decl extends Cmd
 sealed trait Ctrl extends Cmd
 
 case object Labels extends Cmd {
-  def sexpr = List("labels")
   def bexpr = cuvee.undefined
 }
 
 case class SetLogic(logic: String) extends Ctrl {
-  def sexpr = List("set-logic", logic)
   def bexpr =
     List("/* ", "Command unsupported in boogie:", "set-logic", logic, " */")
 }
 
 case class SetOption(attr: String, arg: Any) extends Ctrl {
-  def sexpr = List("set-option", ":" + attr, arg)
   def bexpr = List(
     "/* ",
     "Command unsupported in boogie:",
@@ -78,52 +70,40 @@ case class SetOption(attr: String, arg: Any) extends Ctrl {
 }
 
 case class GetInfo(attr: String) extends Ctrl {
-  def sexpr = List("get-info", ":" + attr)
   def bexpr =
     List("/* ", "Command unsupported in boogie:", "get-info", attr, " */")
 }
 
 case class SetInfo(attr: String, arg: Option[Any]) extends Ctrl {
-  def sexpr = arg match {
-    case None      => List("set-info", ":" + attr)
-    case Some(arg) => List("set-info", ":" + attr, arg)
-  }
-
   def bexpr =
     List("/* ", "Command unsupported in boogie:", "set-info", attr, " */")
 }
 
 case class Push(depth: Int) extends Ctrl {
-  def sexpr = List("push", depth)
   def bexpr = cuvee.undefined
 }
 
 case class Pop(depth: Int) extends Ctrl {
-  def sexpr = List("pop", depth)
   def bexpr = cuvee.undefined
 }
 
 case object GetModel extends Cmd {
-  def sexpr = List("get-model")
   def bexpr = cuvee.undefined
 }
 case object Exit extends Ctrl {
-  def sexpr = List("exit")
   def bexpr = cuvee.undefined
 }
 case object Reset extends Ctrl {
-  def sexpr = List("reset")
   def bexpr = cuvee.undefined
 }
 
 case class Assert(expr: Expr) extends Cmd {
-  def sexpr = List("assert", expr)
   def bexpr = List("assert", " ", expr, ";")
 }
 
 // This is part neither of SMT-LIB nor of Boogie (but we support it there).
-case class Lemma(expr: Expr, tactic: Option[Tactic], assert: Boolean) extends Cmd {
-  def sexpr = List("lemma", expr)
+case class Lemma(expr: Expr, tactic: Option[Tactic], assert: Boolean)
+    extends Cmd {
   def bexpr = tactic match {
     case None         => List("lemma", " ", expr, ";")
     case Some(tactic) => List("lemma", " ", expr, "proof", tactic, ";")
@@ -137,22 +117,24 @@ case class Lemma(expr: Expr, tactic: Option[Tactic], assert: Boolean) extends Cm
 // }
 
 case object CheckSat extends Cmd {
-  def sexpr = List("check-sat")
   def bexpr = cuvee.undefined
 }
 
 case class DeclareSort(name: Name, arity: Int) extends Decl {
-  def sexpr = List("declare-sort", name, arity)
   def bexpr = List("type", " ", name, ";")
 }
-case class DefineSort(name: Name, params: List[Param], body: Type) extends Decl {
-  def sexpr = List("define-sort", name, params, body)
+case class DefineSort(name: Name, params: List[Param], body: Type)
+    extends Decl {
   def bexpr = cuvee.undefined // TODO: Is this right?
 }
 
-case class DeclareFun(name: Name, params: List[Param], args: List[Type], res: Type) extends Decl {
+case class DeclareFun(
+    name: Name,
+    params: List[Param],
+    args: List[Type],
+    res: Type
+) extends Decl {
   // require(params.isEmpty, "generic functions are currently not supported")
-  def sexpr = List("declare-fun", name, args, res)
   def bexpr = List("function", " ", name, "(", args, ")", ":", res)
 }
 
@@ -165,13 +147,6 @@ case class DefineFun(
     rec: Boolean
 ) extends Decl {
   require(params.isEmpty, "generic functions are currently not supported")
-  def sexpr = List(
-    if (rec) "define-fun-rec" else "define-fun",
-    name,
-    formals.asFormals,
-    res,
-    body
-  )
   def bexpr = List(
     "function",
     " ",
@@ -190,8 +165,10 @@ case class DefineFun(
   )
 }
 
-case class DeclareDatatypes(arities: List[(Name, Int)], datatypes: List[Datatype]) extends Decl {
-  def sexpr = List("declare-datatypes", arities, datatypes)
+case class DeclareDatatypes(
+    arities: List[(Name, Int)],
+    datatypes: List[Datatype]
+) extends Decl {
   def bexpr = List(
     "/* ",
     "declare-datatypes",
@@ -210,23 +187,6 @@ case class DeclareProc(
     out: List[Var],
     spec: Option[Spec]
 ) extends Decl {
-  def sexpr = (params, spec) match {
-    case (Nil, None) =>
-      List("declare-proc", in.asFormals, out.asFormals)
-    case (Nil, Some(Spec(mod, pre, post))) =>
-      List(
-        "declare-proc",
-        in.asFormals,
-        out.asFormals,
-        ":modifies",
-        mod,
-        ":precondition",
-        pre,
-        ":postcondition",
-        post
-      )
-  }
-
   def bexpr = cuvee.undefined
 }
 
@@ -238,24 +198,5 @@ case class DefineProc(
     spec: Option[Spec],
     body: Prog
 ) extends Decl {
-  def sexpr = (params, spec) match {
-    case (Nil, None) =>
-      List("define-proc", in.asFormals, out.asFormals, body)
-    case (Nil, Some(Spec(mod, pre, post))) =>
-      List(
-        "declare-proc",
-        in.asFormals,
-        out.asFormals,
-        ":modifies",
-        mod,
-        ":precondition",
-        pre,
-        ":postcondition",
-        post,
-        body
-      )
-
-  }
-
   def bexpr = cuvee.undefined
 }
