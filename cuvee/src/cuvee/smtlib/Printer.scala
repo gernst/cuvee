30920345b73c56d45dadef31f583f19b3470a7d9
Gidon Ernst <gidon.ernst@sosy.ifi.lmu.de>
2023-09-04 16:14:42 -0600

    some more printer tweaks (argh)

--- cuvee/src/cuvee/smtlib/Printer.scala
+++ cuvee/src/cuvee/smtlib/Printer.scala
@@ -14,114 +14,137 @@ class SyntaxList(xs: List[Syntax]) {
 }
 
 object Printer extends cuvee.util.Printer {
+  def just(any: Any): List[String] =
+    List(any.toString)
+
+  def args(args: Any*) = {
+    args.toList
+  }
+
+  def args2(args: Any*)(rest: List[Any]) = {
+    args.toList ++ rest
+  }
+
+  def parens(args: Any*) = {
+    printApp(args.toList flatMap lines)
+  }
+
+  def parens2(args: Any*)(rest: List[Any]) = {
+    printApp(args.toList ++ rest flatMap lines)
+  }
+
   def lines(any: Any): List[String] = any match {
     // Boolean values
-    case true  => List("true")
-    case false => List("false")
-    // Numbers
-    case i: Int    => List(i.toString)
-    case i: BigInt => List(i.toString)
-    case f: Float  => List(f.toString)
+    case true  => just("true")
+    case false => just("false")
+
+    // Basic Types
+    case i: Int    => just(i)
+    case i: BigInt => just(i)
+    case s: String => just(s)
+    case f: Float  => just(f)
+
     // Name
     case n: Name => List(cuvee.sexpr.mangle(n.toLabel))
+
     // smtlib.Cmd
-    case Success                  => lines("success")
-    case Unsupported              => lines("unsupported")
-    case Error(info)              => wrapper2("error")(info)
-    case Sat                      => lines("sat")
-    case Unsat                    => lines("unsat")
-    case Unknown                  => lines("unknown")
-    case Model(defs)              => wrapper2("model")(defs)
-    case Labels                   => wrapper("labels")
-    case SetLogic(logic)          => wrapper("set-logic", logic)
-    case SetOption(attr, arg)     => wrapper("set-option", ":" + attr, arg)
-    case GetInfo(attr)            => wrapper("get-info", ":" + attr)
-    case Push(depth)              => wrapper("push", depth)
-    case Pop(depth)               => wrapper("pop", depth)
-    case GetModel                 => wrapper("get-model")
-    case Exit                     => wrapper("exit")
-    case Reset                    => wrapper("reset")
-    case Assert(expr)             => wrapper("assert", expr)
-    case CheckSat                 => wrapper("check-sat")
-    case Lemma(expr, _, _)        => wrapper("lemma", expr)
-    case SetInfo(attr, None)      => wrapper("set-info", ":" + attr)
-    case SetInfo(attr, Some(arg)) => wrapper("set-info", ":" + attr, arg)
+    case Success                  => just("success")
+    case Unsupported              => just("unsupported")
+    case Error(info)              => parens2("error")(info)
+    case Sat                      => just("sat")
+    case Unsat                    => just("unsat")
+    case Unknown                  => just("unknown")
+    case Model(defs)              => parens2("model")(defs)
+    case Labels                   => parens("labels")
+    case SetLogic(logic)          => parens("set-logic", logic)
+    case SetOption(attr, arg)     => parens("set-option", ":" + attr, arg)
+    case GetInfo(attr)            => parens("get-info", ":" + attr)
+    case Push(depth)              => parens("push", depth)
+    case Pop(depth)               => parens("pop", depth)
+    case GetModel                 => parens("get-model")
+    case Exit                     => parens("exit")
+    case Reset                    => parens("reset")
+    case Assert(expr)             => parens("assert", expr)
+    case CheckSat                 => parens("check-sat")
+    case Lemma(expr, _, _)        => parens("lemma", expr)
+    case SetInfo(attr, None)      => parens("set-info", ":" + attr)
+    case SetInfo(attr, Some(arg)) => parens("set-info", ":" + attr, arg)
 
     case DeclareSort(name, arity) =>
-      wrapper("declare-sort", name, arity)
+      parens("declare-sort", name, arity)
     case DefineSort(name, params, body) =>
-      wrapper("define-sort", name, params, body)
+      parens("define-sort", name, params, body)
     case DeclareFun(name, params, args, res) =>
-      wrapper("declare-fun", name, args, res)
+      parens("declare-fun", name, args, res)
     case DefineFun(name, params, formals, res, body, rec) if rec =>
-      wrapper("define-fun-rec", name, formals.asFormals, res, body)
+      parens("define-fun-rec", name, formals.asFormals, res, body)
     case DefineFun(name, params, formals, res, body, _) =>
-      wrapper("define-fun", name, formals.asFormals, res, body)
+      parens("define-fun", name, formals.asFormals, res, body)
     case DeclareDatatypes(arities, datatypes) =>
-      wrapper("declare-datatypes", arities, datatypes)
-    case DeclareProc(name, params, in, out, spec) =>
-      (params, spec) match {
-        case (Nil, None) => wrapper("declare-proc", in.asFormals, out.asFormals)
-        case (Nil, Some(Spec(mod, pre, post))) =>
-          wrapper(
-            "declare-proc",
-            in.asFormals,
-            out.asFormals,
-            ":modifies",
-            mod,
-            ":precondition",
-            pre,
-            ":postcondition",
-            post
-          )
-      }
-    case DefineProc(name, params, in, out, spec, body) =>
-      (params, spec) match {
-        case (Nil, None) =>
-          wrapper("define-proc", in.asFormals, out.asFormals, body)
-        case (Nil, Some(Spec(mod, pre, post))) =>
-          wrapper(
-            "define-proc",
-            in.asFormals,
-            out.asFormals,
-            ":modifies",
-            mod,
-            ":precondition",
-            pre,
-            ":postcondition",
-            post,
-            body
-          )
-      }
+      parens("declare-datatypes", arities, datatypes)
+
+    case DeclareProc(name, params, in, out, None) =>
+      parens("declare-proc", in.asFormals, out.asFormals)
+
+    case DeclareProc(name, params, in, out, Some(Spec(mod, pre, post))) =>
+      parens(
+        "declare-proc",
+        in.asFormals,
+        out.asFormals,
+        ":modifies",
+        mod,
+        ":precondition",
+        pre,
+        ":postcondition",
+        post
+      )
+
+    case DefineProc(name, Nil, in, out, None, body) =>
+      parens("define-proc", in.asFormals, out.asFormals, body)
+
+    case DefineProc(name, Nil, in, out, Some(Spec(mod, pre, post)), body) =>
+      parens(
+        "define-proc",
+        in.asFormals,
+        out.asFormals,
+        ":modifies",
+        mod,
+        ":precondition",
+        pre,
+        ":postcondition",
+        post,
+        body
+      )
+
     // pure.Expr
+    case Lit(a, _)             => just(a)
     case Var(name, _)          => lines(name)
-    case Lit(a, _)             => lines(a.toString)
-    case Is(arg, fun)          => wrapper(List("_", "is", fun.name), arg)
-    case Case(pat, expr)       => wrapper(pat, expr)
-    case Match(expr, cases, _) => wrapper("match", expr, cases)
-    case LetEq(x, e)           => wrapper(x, e)
-    case Let(eqs, body)        => wrapper("let", eqs, body)
-    case Note(expr, attr)      => wrapper2("!", expr)(attr)
-    case Distinct(exprs)       => wrapper2("distinct")(exprs)
+    case Is(arg, fun)          => parens(args("_", "is", fun.name), arg)
+    case Case(pat, expr)       => parens(pat, expr)
+    case Match(expr, cases, _) => parens("match", expr, cases)
+    case LetEq(x, e)           => parens(x, e)
+    case Let(eqs, body)        => parens("let", eqs, body)
+    case Note(expr, attr)      => parens2("!", expr)(attr)
+    case Distinct(exprs)       => parens2("distinct")(exprs)
 
     case Bind(quant, formals, body, _) =>
-      wrapper(quant.name, formals.asFormals, body)
+      parens(quant.name, formals.asFormals, body)
 
     case App(inst, Nil) =>
       lines(inst)
 
     // constant array
-    case Const(arg) => wrapper(List("as", "const", arg.typ), arg)
+    case Const(arg) => parens(args("as", "const", arg.typ), arg)
 
-    case And(phis) => wrapper2("and")(phis)
-    case Or(phis)  => wrapper2("or")(phis)
+    case And(phis) => parens2("and")(phis)
+    case Or(phis)  => parens2("or")(phis)
 
     case App(inst, args) =>
-      wrapper2(inst.fun.name)(args)
+      parens2(inst.fun.name)(args)
 
     // pure.Fun
     case Inst(fun, ty) if fun.params.nonEmpty =>
-      wrapper("as", fun.name, fun.res subst ty)
+      parens("as", fun.name, fun.res subst ty)
     case Inst(fun, ty) =>
       lines(fun.name)
 
@@ -129,15 +152,15 @@ object Printer extends cuvee.util.Printer {
     case Atom(expr, _) =>
       lines(expr)
     case Conj(xs, neg) =>
-      wrapper2("exists", xs.asFormals, "or")(neg)
+      parens2("exists", xs.asFormals, "or")(neg)
     case Disj(xs, neg, pos) =>
-      wrapper("forall", xs.asFormals, wrapper("=>", wrapper2("and")(neg), wrapper2("or")(pos)))
+      parens("forall", xs.asFormals, args("=>", args2("and")(neg), args2("or")(pos)))
 
     // pure.Type
     case Datatype(params, constrs) =>
       val constrs_ = for ((k, sels) <- constrs) yield {
         val sels_ = for (sel <- sels) yield {
-          List(sel.name, sel.res)
+          args(sel.name, sel.res)
         }
         k.name :: sels_
       }
@@ -145,41 +168,37 @@ object Printer extends cuvee.util.Printer {
       if (params.isEmpty)
         lines(constrs_)
       else
-        wrapper("par", params, constrs_)
-    case Param(name)     => lines(name)
-    case Sort(con, Nil)  => lines(con.name)
-    case Sort(con, args) => wrapper2(con.name)(args)
+        parens("par", params, constrs_)
+
+    case Param(name)    => lines(name)
+    case Sort(con, Nil) => lines(con.name)
+
+    case Sort(con, args) => parens2(con.name)(args)
+
     // imp.Prog
-    case Block(progs)        => wrapper2("block")(progs)
-    case Break               => wrapper("break")
-    case Return              => wrapper("return")
-    case Local(xs, rhs)      => wrapper("local", (xs.asFormals zip rhs))
-    case Assign(xs, rhs)     => wrapper("assign", xs zip rhs)
-    case Spec(xs, pre, post) => wrapper("spec", xs, pre, post)
-    case Call(name, in, out) => wrapper("call", name, in, out)
+    case Block(progs)        => parens2("block")(progs)
+    case Break               => parens("break")
+    case Return              => parens("return")
+    case Local(xs, rhs)      => parens("local", (xs.asFormals zip rhs))
+    case Assign(xs, rhs)     => parens("assign", xs zip rhs)
+    case Spec(xs, pre, post) => parens("spec", xs, pre, post)
+    case Call(name, in, out) => parens("call", name, in, out)
+
     case If(test, left, right) =>
-      wrapper("if", test, left, right) // TODO typo? was "id" in Prog (yes)
+      parens("if", test, left, right)
+
     case While(test, body, _, inv, sum, _) =>
-      wrapper("while", test, body, ":invariant", inv, ":summary", sum)
+      parens("while", test, body, ":invariant", inv, ":summary", sum)
+
     // sexpr.Expr
     case lit: sexpr.Lit  => lines(lit.toString) // TODO which toString is this?
-    case sexpr.Kw(name)  => wrapper(":" + name)
-    case sexpr.Id(name)  => wrapper(name)
-    case sexpr.App(args) => wrapper(args)
+    case sexpr.Kw(name)  => parens(":" + name)
+    case sexpr.Id(name)  => parens(name)
+    case sexpr.App(args) => parens(args)
 
-    case s: Syntax => lines(s.sexpr)
-    case s: String => List(s)
     // Applications, either represented by a pair (a, b) or a list
     case (a, b)      => printApp(lines(a) ++ lines(b))
     case xs: List[_] => printApp(xs flatMap lines)
-  }
-
-  def wrapper(args: Any*) = {
-    printApp(args.toList flatMap lines)
-  }
-
-  def wrapper2(args: Any*)(rest: List[Any]) = {
-    printApp(args.toList ++ rest flatMap lines)
   }
 
   def printApp(args: List[String]): List[String] = {
