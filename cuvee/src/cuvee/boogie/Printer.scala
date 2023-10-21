596f1860127a02e8ab01b9629187d93e481213f9
Hannah <knie.hannah@campus.lmu.de>
2023-10-21 12:21:18 +0200

    added Cmd to boogie Printer

--- cuvee/src/cuvee/boogie/Printer.scala
+++ cuvee/src/cuvee/boogie/Printer.scala
@@ -7,12 +7,101 @@ import cuvee.util.Name
 import cuvee.smtlib.Res
 import cuvee.smtlib
 import cuvee.imp.Prog
+import cuvee.smtlib._
 
 trait Syntax extends util.Syntax {
   def bexpr: List[Any]
 }
 
 object Printer extends cuvee.util.Printer {
+
+  def lines(cmd: Cmd): List[String] = cmd match {
+    case Labels => cuvee.undefined
+    case SetLogic(logic) =>
+      List("/* ", "Command unsupported in boogie:", "set-logic", logic, " */")
+    case SetOption(attr, arg) =>
+      List(
+        "/* ",
+        "Command unsupported in boogie:",
+        "set-option",
+        attr,
+        arg.toString,
+        " */"
+      )
+    case GetInfo(attr) =>
+      List("/* ", "Command unsupported in boogie:", "get-info", attr, " */")
+    case SetInfo(attr, _) =>
+      List("/* ", "Command unsupported in boogie:", "set-info", attr, " */")
+    case Push(depth) => cuvee.undefined
+    case Pop(depth)  => cuvee.undefined
+    case GetModel    => cuvee.undefined
+    case Exit        => cuvee.undefined
+    case Reset       => cuvee.undefined
+    case Assert(expr) =>
+      expr match {
+        case Bind(quant, formals, body, _) =>
+          List(
+            "axiom " + quant.name + formals
+              .map(_.toStringTyped)
+              .mkString(" ", ", ", " :: "),
+            body + ";"
+          )
+        case _ =>
+          List("axiom " + expr + ";")
+      }
+    case Lemma(expr, tactic, _) =>
+      tactic match {
+        case None         => List("lemma " + expr + ";")
+        case Some(tactic) => List("lemma " + expr, "proof" + tactic + ";")
+      }
+    case CheckSat                  => cuvee.undefined
+    case DeclareSort(name, arity)  => List("type " + name + ";") // add params
+    case DefineSort(name, _, body) => List("type " + name + " = " + body + ";")
+    case x @ DeclareFun(name, params, _, res) =>
+      List(
+        "function " + name + "(" + x.formals.toStringTyped + "): " + res + ";"
+      )
+    case DefineFun(name, _, formals, res, body, _) =>
+      List(
+        "function " + name + "(" + formals.toStringTyped + "): " + res + "{",
+        "  " + body,
+        "}"
+      )
+    case DeclareDatatypes(arities, datatypes) =>
+      val lines = arities zip datatypes map {
+        case ((name, arity), Datatype(Nil, constrs)) =>
+          val cs = constrs map {
+            case (constr, Nil) =>
+              constr.name
+            case (constr, sels) =>
+              val as = sels map { case Fun(name, _, _, res) =>
+                name + ": " + res
+              }
+              constr.name + as.mkString("(", ", ", ")")
+          }
+          name + cs.mkString(" = ", " | ", ";")
+
+        case ((name, arity), Datatype(params, constrs)) =>
+          val cs = constrs map {
+            case (constr, Nil) =>
+              constr.name
+            case (constr, sels) =>
+              val as = sels map { case Fun(name, _, _, res) =>
+                name + ": " + res
+              }
+              constr.name + as.mkString("(", ", ", ")")
+          }
+          name + params.mkString("<", ", ", ">") + cs.mkString(
+            " = ",
+            " | ",
+            ";"
+          )
+      }
+      "data " :: lines
+    case DeclareProc(name, params, in, out, spec)      => cuvee.undefined
+    case DefineProc(name, params, in, out, spec, body) => cuvee.undefined
+  }
+
   def lines(prog: Prog): List[String] = {
     ???
   }
@@ -26,9 +115,9 @@ object Printer extends cuvee.util.Printer {
     case i: BigInt => List(i.toString)
     case f: Float  => List(f.toString)
     // Name
-    case n: Name => List(n.toLabel)
+    case n: Name           => List(n.toLabel)
     case smtlib.Error(msg) => List(msg.mkString("\"", " ", "\""))
-    case res: Res => List(res.toString())
+    case res: Res          => List(res.toString())
     // Props
     case p: Prop => (p.bexpr map (_.toString))
     // Syntax (recursive call on the syntax' s-expression)
